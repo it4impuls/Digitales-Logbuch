6193ca9d51182e6a20292ee29451c5387f710dac
marilynias <marilynia57+github@gmail.com>
2024-06-18 15:08:28 +0200

    nope

--- digi_log_be/config/gunicorn.config.py
+++ digi_log_be/config/gunicorn.config.py
@@ -1,7 +1,7 @@
 """Gunicorn *development* config file"""
 
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
-wsgi_app = "digi_log:application"
+wsgi_app = "digi_log.wsgi:application"
 # The granularity of Error log outputs
 loglevel = "debug"
 # The number of worker processes for handling requests
