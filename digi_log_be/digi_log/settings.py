3b10e0e2199614876038d716937a14c086f00fda
marilynias <marilynia57+github@gmail.com>
2024-06-18 15:18:26 +0200

    allow prod domain name correct branch name

--- digi_log_be/digi_log/settings.py
+++ digi_log_be/digi_log/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-=u-15y)c1y&z22*o^9n*h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1", "kursplan.impulsreha.local"]
 
 LOGIN_URL = "/api/login/"
 
