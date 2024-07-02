1ddfdb888aa4f7cd9258714b358a2a341af6ee84
marilynias <marilynia57+github@gmail.com>
2024-07-02 13:55:08 +0200

    dynamic port/ip

--- digi_log_fe/src/environments/environment.prod.ts
+++ digi_log_fe/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // $ENV is defined in src/typings.d.ts, var gotten from custom-webpack.config.js
-  BACKEND_PORT: $ENV.BACKEND_PORT,
-  BACKEND_IP: $ENV.BACKEND_IP
+  BACKEND_PORT: 8000,
+  BACKEND_IP: "localhost"
 };
