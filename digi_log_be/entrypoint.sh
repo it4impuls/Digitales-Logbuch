bfec98991ff49d22e85fbeeddc532e1706b086d4
marilynias <marilynia57+github@gmail.com>
2024-07-04 09:05:01 +0200

    print error

--- digi_log_be/entrypoint.sh
+++ digi_log_be/entrypoint.sh
@@ -1,5 +1,5 @@
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --noinput || echo 'couldnt create su'
+python manage.py createsuperuser --noinput
 gunicorn -c ./config/gunicorn.config.py
