57e69601ed732dd15265328f7e7e70794eba2c8e
marilynias <marilynia57+github@gmail.com>
2024-07-04 09:48:24 +0200

    get or create

--- digi_log_be/digilog_backend/management/commands/import.py
+++ digi_log_be/digilog_backend/management/commands/import.py
@@ -41,5 +41,5 @@ class Command(BaseCommand):
                 e["material"] = entry.get("Material/Unterlagen", "")
                 e["dates"] = entry.get("Wie oft wird der Kurs angeboten", "")
                 e["duration"] = entry.get("Kursdauer", "")
-                course = Course.objects.create(**e)
+                course = Course.objects.get_or_create(title=e["title"], defaults=e)
                 print(course)
--- digi_log_be/entrypoint.sh
+++ digi_log_be/entrypoint.sh
@@ -2,4 +2,5 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput
+#python manage.py import --file example.json
 gunicorn -c ./config/gunicorn.config.py
