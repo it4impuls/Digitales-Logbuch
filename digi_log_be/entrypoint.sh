c81b8299c7b1fc5b3d4c1e50db5a6b3ac793d3e9
marilynias <marilynia57+github@gmail.com>
2024-07-04 08:49:26 +0200

    dont echo...

--- digi_log_be/entrypoint.sh
+++ digi_log_be/entrypoint.sh
@@ -1,4 +1,4 @@
 
 python manage.py migrate
-echo python manage.py createsuperuser --noinput || echo 'couldnt create su'
+python manage.py createsuperuser --noinput || echo 'couldnt create su'
 gunicorn -c ./config/gunicorn.config.py
