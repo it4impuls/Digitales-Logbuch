b82d80f9138570ade84b7497234a0b98c1d81653
marilynias <marilynia57+github@gmail.com>
2024-07-05 13:05:22 +0200

    debug

--- digi_log_be/digilog_backend/management/commands/import.py
+++ digi_log_be/digilog_backend/management/commands/import.py
@@ -41,6 +41,7 @@ class Command(BaseCommand):
                 e["material"] = entry.get("Material/Unterlagen", "")
                 e["dates"] = entry.get("Wie oft wird der Kurs angeboten", "")
                 e["duration"] = entry.get("Kursdauer", "")
-                print([(key, len(val)) for key, val in e.items() if key != "host"])
+                print([(key, len(str(val))) for key, val in e.items()])
+                print(e)
                 course = Course.objects.get_or_create(title=e["title"], defaults=e)
                 print(course)
