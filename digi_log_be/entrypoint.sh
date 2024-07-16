8f8e8043a368bec4dad34f2f0bf241764231956f
marilynias <marilynia57+github@gmail.com>
2024-07-16 10:34:21 +0200

    test after migrating

--- digi_log_be/entrypoint.sh
+++ digi_log_be/entrypoint.sh
@@ -1,6 +1,2 @@
 
-python manage.py makemigrations
-python manage.py migrate
-python manage.py createsuperuser --noinput
-#python manage.py import --file example.json
 gunicorn -c ./config/gunicorn.config.py
